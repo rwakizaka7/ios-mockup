81eabc81848b8cefa31496cdea040f2543ddda76
脇坂亮太 <rwakizaka7@yahoo.co.jp>
2018-06-03 17:10:53 +0900

    Objective-Cの設定を追加する。

---
+++ ios-mockup/URLSession_Objctive_C/URLSession_Objective_C_ViewController.h
@@ -0,0 +1,17 @@
+//
+//  URLSession_ViewController.h
+//  ios-mockup
+//
+//  Created by 脇坂亮太 on 2018/06/03.
+//  Copyright © 2018年 アルサーガパートナーズ株式会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ インターフェースの宣言
+ 外部アクセス可能なメンバ変数やメソッドを宣言する。
+ */
+@interface URLSession_Objective_C_ViewController: UIViewController
+
+@end
---
+++ ios-mockup/URLSession_Objctive_C/URLSession_Objective_C_ViewController.m
@@ -0,0 +1,28 @@
+//
+//  URLSession_ViewController.m
+//  ios-mockup
+//
+//  Created by 脇坂亮太 on 2018/06/03.
+//  Copyright © 2018年 アルサーガパートナーズ株式会社. All rights reserved.
+//
+
+#import "URLSession_Objective_C_ViewController.h"
+#import <Foundation/Foundation.h>
+
+/**
+ インターフェースの宣言
+ 外部アクセス不可なメンバ変数やメソッドを宣言する。
+ */
+@interface URLSession_Objective_C_ViewController ()
+
+@end
+
+/**
+ クラスの定義
+ メソッドの処理を書く。
+ */
+@implementation URLSession_Objective_C_ViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+@end
